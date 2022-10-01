27a9fa264540b555fcda92a122147f736527868e
Prathamesh Jagtap <101258912+prathameshrjagtap@users.noreply.github.com>
2022-10-01 17:13:42 +0530

    Create Main.java

---
+++ Java-Tutorials/CheckInputIntorNot/Main.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+class Main {
+  public static void main(String[] args) {
+
+    System.out.println("Enter Your Number: ");
+    Scanner sc = new Scanner(System.in);
+    System.out.println(sc.hasNextInt());
+  }
+}
