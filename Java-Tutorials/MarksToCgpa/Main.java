a055e82647edf94472acc368ab3b6e5d408859a7
Prathamesh Jagtap <101258912+prathameshrjagtap@users.noreply.github.com>
2022-10-01 17:21:15 +0530

    Create Main.java

---
+++ Java-Tutorials/MarksToCgpa/Main.java
@@ -0,0 +1,12 @@
+class Main {
+  public static void main(String[] args) {
+    float sub1 = 45;
+    float sub2 = 74;
+    float sub3 = 56;
+    float sum;
+    float cgpa;
+    sum = sub1 + sub2 + sub3;
+    cgpa = sum / 30;
+    System.out.println(cgpa);
+  }
+}
